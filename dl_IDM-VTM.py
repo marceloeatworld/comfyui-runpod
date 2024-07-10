910ba13e10fbbe7f5057a72222a8c73cc22f0d00
marceloeatworld <20625497+marceloeatworld@users.noreply.github.com>
2024-07-10 16:23:59 +0000

    refactor: Update dl_IDM-VTM.py to use new weights path

--- dl_IDM-VTM.py
+++ dl_IDM-VTM.py
@@ -1,9 +1,8 @@
 import os.path
 from huggingface_hub import snapshot_download
 
-CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
-WEIGHTS_PATH = os.path.join(CURRENT_PATH, "/workspace/ComfyUI/models")
-HF_REPO_ID = "yisol/IDM-VTON"
+CURRENT_PATH = os.path.abspath(__file__)
+WEIGHTS_PATH = os.path.join("/workspace/ComfyUI/models", "yisol/IDM-VTON")
 
 if __name__ == "__main__":
-    snapshot_download(repo_id=HF_REPO_ID, local_dir=WEIGHTS_PATH)
+    snapshot_download(repo_id="yisol/IDM-VTON", local_dir=WEIGHTS_PATH)
